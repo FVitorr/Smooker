f508c2f696e9439903015b641e18ee971f1300ae
FVitorr <augustovitor96@gmail.com>
2022-06-21 13:21:16 -0300

    Add Script created products

--- script.js
+++ script.js
@@ -13,3 +13,52 @@ function nextImg(){
     }
     document.getElementById("radio"+count).checked = true;
 }
+
+/* Criar produto de destaque*/
+
+let father = document.getElementById("container_");
+function createDiv(attribute,value){
+    let div = document.createElement("div");
+    div.setAttribute(attribute,value);
+    return div;
+}
+function createImg(attribute,value){
+    let div = document.createElement("img");
+    div.setAttribute(attribute,value);
+    return div;
+}
+let valuer = 25.00
+function createProdut(urlImg,description,valuer,checked=''){
+    let div_conteiner = createDiv('class',"container");
+    let div_product_emphasis = createDiv('class',"product_emphasis");
+    let img = createImg("src",urlImg);
+    let p = document.createElement("p");
+    p.innerHTML = description;
+    let h6 = document.createElement("h6");
+    h6.innerHTML = "R$" + parseFloat(valuer);
+    let ul = document.createElement("ul");
+    for (let j = 0 ; j < 5; j++){
+        let li = document.createElement("li");
+        let i = document.createElement("i");
+        i.setAttribute('class','fa-solid fa-star');
+        li.appendChild(i);
+        ul.appendChild(li);
+    }
+    let spam = document.createElement("spam");
+    spam.setAttribute("class","qtd_aval");
+    spam.innerHTML="(1000)"
+    
+    let button = document.createElement("button");
+    button.setAttribute("class","btn");
+    button.innerHTML="Comprar Agora";
+    
+    let emphasis = [img,p,h6,ul,spam,button]
+    for (let j = 0; j < emphasis.length ;j++){
+        div_product_emphasis.appendChild(emphasis[j]);
+    }
+    div_conteiner.appendChild(div_product_emphasis);
+    father.appendChild(div_conteiner);
+}
+createProdut('https://t4.ftcdn.net/jpg/02/57/72/69/360_F_257726951_n0gIjSVw93lpvdH8UJDooMltieeSXfcB.jpg',
+'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+valuer,'checked');
